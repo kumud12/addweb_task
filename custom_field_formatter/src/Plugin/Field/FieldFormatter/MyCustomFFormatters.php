13752f2db0ee042575399963ed6a073b9bd5ae79
kumud12 <kumud.mca210786@gmail.com>
2023-05-03 14:41:09 +0100

    Create MyCustomFFormatters.php

---
+++ custom_field_formatter/src/Plugin/Field/FieldFormatter/MyCustomFFormatters.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\custom_field_formatter\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Field\FieldItemListInterface;
+
+/**
+* Plugin implementation of the 
+*    'MyCustomFormattera' formatter.
+*
+* @FieldFormatter(
+*   id = "custom_s_ff_my_custom_formatter",
+*   label = @Translation("Field Formatter - Italic style"),
+*   field_types = {
+*     "string_long",
+*     "string"
+*   }
+* )
+*/
+class MyCustomFFormatters extends FormatterBase  {
+
+	public function viewElements(FieldItemListInterface $items, $langcode)
+	{
+		$elements2 = [];
+		$servicedata = "";
+		
+		//Cheking if service started or not
+		if (\Drupal::hasService('custom_field_formatter.text_slugify_service') ) {
+			$servicedata = \Drupal::service('custom_field_formatter.text_slugify_service');
+		}
+		else{
+			\Drupal::logger('Field Formatter Services')->notice('You have requested a non-existent service');
+		}
+
+		foreach($items as $delta => $item){
+
+		//Fetching value from the services function
+		$value = $servicedata->createUrlSlug($item->value);
+		
+			$elements2[$delta] = [
+				'#markup' => "Italic style -> <i>$value</i>",
+			];
+		}
+		return $elements2;
+	}
+
+}
+
